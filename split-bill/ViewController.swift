cd293983a6eecbc5f7d7848473d42d1c3f5d6bed
Wildan <wildanalifrio@gmail.com>
2025-02-24 17:22:22 +0700

    Initial Commit

---
+++ split-bill/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  split-bill
+//
+//  Created by Wildan on 24/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
